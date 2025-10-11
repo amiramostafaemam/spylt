62e3ef48d98951a558eba90edf57b0e585ccada7
amiramostafaemam <amiraaemaam62@gmail.com>
2025-10-11 14:19:05 +0300

    Responsive Testimonial Section

--- src/constants/index.js
+++ src/constants/index.js
@@ -41,11 +41,11 @@ const nutrientLists = [
 
 const cards = [
   {
-    src: "/videos/f1.mp4",
-    rotation: "rotate-z-[-8deg]",
-    name: "Madison",
-    img: "/images/p1.png",
-    translation: "translate-y-[-4%]",
+    src: "/videos/f7.mp4",
+    rotation: "rotate-z-[12deg]",
+    name: "Melisa",
+    img: "/images/p7.png",
+    translation: "translate-y-[5%]",
   },
   {
     src: "/videos/f2.mp4",
@@ -83,58 +83,11 @@ const cards = [
     translation: "translate-y-[4%]",
   },
   {
-    src: "/videos/f7.mp4",
-    rotation: "rotate-z-[12deg]",
-    name: "Melisa",
-    img: "/images/p7.png",
-    translation: "translate-y-[5%]",
+    src: "/videos/f1.mp4",
+    rotation: "rotate-z-[-8deg]",
+    name: "Madison",
+    img: "/images/p1.png",
+    translation: "translate-y-[-4%]",
   },
 ];
-// {
-//   src: "/videos/f1.mp4",
-//   rotation: "rotate-z-[-20deg]",
-//   name: "Madison",
-//   img: "/images/p1.png",
-//   translation: "translate-y-[-5%]",
-// },
-// {
-//   src: "/videos/f2.mp4",
-//   rotation: "rotate-z-[10deg]",
-//   name: "Alexander",
-//   img: "/images/p2.png",
-// },
-// {
-//   src: "/videos/f3.mp4",
-//   rotation: "rotate-z-[-8deg]",
-//   name: "Andrew",
-//   img: "/images/p3.png",
-//   translation: "translate-y-[-5%]",
-// },
-// {
-//   src: "/videos/f4.mp4",
-//   rotation: "rotate-z-[6deg]",
-//   name: "Bryan",
-//   img: "/images/p4.png",
-//   translation: "translate-y-[5%]",
-// },
-// {
-//   src: "/videos/f5.mp4",
-//   rotation: "rotate-z-[-10deg]",
-//   name: "Chris",
-//   img: "/images/p5.png",
-// },
-// {
-//   src: "/videos/f6.mp4",
-//   rotation: "rotate-z-[4deg]",
-//   name: "Devante",
-//   img: "/images/p6.png",
-//   translation: "translate-y-[5%]",
-// },
-// {
-//   src: "/videos/f7.mp4",
-//   rotation: "rotate-z-[20deg]",
-//   name: "Melisa",
-//   img: "/images/p7.png",
-//   translation: "translate-y-[10%]",
-// },
 export { flavorlists, nutrientLists, cards };
