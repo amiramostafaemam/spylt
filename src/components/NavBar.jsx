c65996ddd1e69de1415ec65b2d3e76f3594b7cde
amiramostafaemam <amiraaemaam62@gmail.com>
2025-10-13 14:24:23 +0300

    Navbar edition

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0 z-50 md:p-7 p-3 ">
-      <img src="/images/nav-logo.svg" alt="nav-logo" className="w-19" />
+    <nav className="fixed top-0 left-0 z-50 md:p-8 p-3 ">
+      <img src="/images/nav-logo.svg" alt="nav-logo" className="w-17" />
     </nav>
   );
 };
