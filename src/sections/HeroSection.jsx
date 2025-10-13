f83f78a14e587145ccf6b26ed794d3f5032bb662
amiramostafaemam <amiraaemaam62@gmail.com>
2025-10-13 15:34:04 +0300

    deploy 3rd edition

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -36,6 +36,7 @@ const HeroSection = () => {
           titleSplit.chars,
           {
             yPercent: 200,
+            opacity: 0,
             stagger: 0.02,
             ease: "power2.out",
           },
